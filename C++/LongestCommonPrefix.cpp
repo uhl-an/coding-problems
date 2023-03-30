0981b1589b37430b25838560c35936eb911ef754
uhl-an <robotuhlan@gmail.com>
2023-03-30 10:38:29 -0400

    [Coding Practice][C++] Solved Longest Common Prefix - 03.30.2023

---
+++ C++/LongestCommonPrefix.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+/**
+ * Solved: 03.30.2023
+ * Problem:
+ * :Write a function to find the longest common prefix string amongst an array of strings.
+ *      If there is no common prefix, return an empty string "".
+*/
+class Solution
+{
+public:
+    // finds the longest common prefix by incrementally concatenating characters
+    //      until an an uncommon character is encountered
+    std::string longestCommonPrefix(std::vector<std::string>& strs)
+    {
+        std::string longestCommonPrefix = "";
+
+        // take the first string and for every character
+        //      compare it to the character in the same position
+        //      of every other word; if the current character
+        //      is equal to the character in the same position
+        //      of every other word, append it to the returning string, 
+        //      otherwise, return the current running prefix.
+        for(int i = 0; i < strs.at(0).length(); i++)
+        {
+            for(int j = 0; j < strs.size(); j++)
+            {
+                if(i >= strs.at(j).length()) return longestCommonPrefix;
+                else if(strs.at(0).at(i) != strs.at(j).at(i)) return longestCommonPrefix;
+                else if(j == strs.size() - 1) longestCommonPrefix += strs.at(0).at(i);
+            }
+        }
+
+        return longestCommonPrefix;        
+    }
+};
+
+int main()
+{
+    std::vector<std::string> data = { "flower", "flow", "flight" };
+
+    Solution s;
+
+    std::cout << "Longest Common Prefix----" << std::endl;
+    std::cout << "[flower, flow, flight] : " << s.longestCommonPrefix(data) << std::endl;
+
+    data = { "flower", "flowey", "flow", "flowering" };
+    std::cout << "[flower, flowey, flow, flowering] : " << s.longestCommonPrefix(data) << std::endl;
+
+    data = { "dog" , "racecar" , "car"};
+    std::cout << "[dog, racecar, car] : " << s.longestCommonPrefix(data) << std::endl;
+
+    return 0;  
+}
+
--- C++/RomanToInteger.cpp
+++ C++/RomanToInteger.cpp
@@ -1,4 +1,76 @@
+#include <string>
+#include <iostream>
 
+/**
+ * Solved: 03.30.2023
+ * Problem:
+ * :Given a roman numeral, convert it to an integer.
+*/
+// Stores Roman Numeral conversions
+enum Numerals : int
+{
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000
+};
+
+class Solution 
+{
+public:
+
+    // converts a given roman numeral character to it's
+    //      decimal equivalent as an integer.
+    // Returns -1 if it's not a roman numeral.
+    int integerEquivalent(char romanNumeral)
+    {
+        switch(romanNumeral)
+        {
+            case 'I': return Numerals::I;
+            case 'V': return Numerals::V;
+            case 'X': return Numerals::X;
+            case 'L': return Numerals::L;
+            case 'C': return Numerals::C;
+            case 'D': return Numerals::D;
+            case 'M': return Numerals::M;
+        }
+
+        return -1;
+    }
+
+    // converts roman numeral to integer equivalent by
+    //      reading the roman numeral string backward, adding
+    //      or subtracting from the numeral sum as necessary.
+    int romanToInt(std::string s)
+    {
+        // intializes the previous value to the decimal equivalent
+        //       of the last roman numeral
+        int prevValue = integerEquivalent(s.at(s.length() - 1));
+        int numeralSum = prevValue;
+
+        // starting from the second to last character in the string,
+        //      iterate through the string converting each roman character
+        //      to it's decimal equivalent; if the current roman numeral
+        //      is less than the previous(signaling a subtraction) subtract
+        //      the decimal value of the current numeral from the numeral sum;
+        //      otherwise, add the decumal value of the current numeral to the
+        //      numeral sum.
+        for(int i = s.length() - 2; i >= 0; i--)
+        {
+            int currentValue = integerEquivalent(s.at(i));
+
+            if(currentValue < prevValue) numeralSum -= currentValue;
+            else numeralSum += currentValue;
+
+            prevValue = currentValue;
+        }
+
+        return numeralSum;
+    }
+};
 
 int main()
 {
