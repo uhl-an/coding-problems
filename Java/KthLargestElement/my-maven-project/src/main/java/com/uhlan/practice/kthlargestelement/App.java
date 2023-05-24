b669fc3a7e8e41baeed6e6bab6212d1ffc33b389
uhl-an <robotuhlan@gmail.com>
2023-05-23 21:44:49 -0400

    [Coding Practice][Java] Fixed indexing issue with Kth Largest Element

--- Java/KthLargestElement/my-maven-project/src/main/java/com/uhlan/practice/kthlargestelement/App.java
+++ Java/KthLargestElement/my-maven-project/src/main/java/com/uhlan/practice/kthlargestelement/App.java
@@ -19,13 +19,13 @@ class KthLargest
         boolean inserted = false;
         for(int i = 0; i < newNums.length; i++)
         {
-            if(inserted || i == nums.length)
+            if(inserted)
             {
                 newNums[i] = nums[i - 1];
             }
             else
             {
-                if(val < nums[i])
+                if(nums.length == 0 || i == nums.length || val < nums[i])
                 {
                     newNums[i] = val;
                     inserted = true;
@@ -39,7 +39,7 @@ class KthLargest
 
         nums = newNums;
 
-        return nums[nums.length - k];
+        return nums.length > k ? nums[nums.length - k] : nums[0];
     }
 }
 
@@ -53,6 +53,15 @@ public class App
         System.out.println(kthLargest.Add(5));   // returns 5
         System.out.println(kthLargest.Add(10));  // returns 5
         System.out.println(kthLargest.Add(9));   // returns 8
-        System.out.println(kthLargest.Add(4));   // returns 8
+        System.out.println(kthLargest.Add(4)); // returns 8
+        
+        // data: [[1,[]],[-3],[-2],[-4],[0],[4]]
+        int[] nums2 = {};
+        KthLargest kthLargest2 = new KthLargest(1, nums2);
+        System.out.println(kthLargest2.Add(-3)); // returns -3
+        System.out.println(kthLargest2.Add(-2)); // returns -2
+        System.out.println(kthLargest2.Add(-4)); // returns -2
+        System.out.println(kthLargest2.Add(0)); // returns 0
+        System.out.println(kthLargest2.Add(4)); // returns 4
     }
 }
